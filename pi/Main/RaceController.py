b3fccc9567af3c651ea4eeabfcf4129977ea0098
Tadejash <71354069+Tadejash@users.noreply.github.com>
2026-04-19 04:52:32 +0100

    fixed spacing

--- pi/Main/RaceController.py
+++ pi/Main/RaceController.py
@@ -12,7 +12,7 @@
 # "start track"           - Resumes path following
 # "flip"                  - Executes robot flip
 # "celebrate"             - Triggers celebration routine
-# "start point <x> <y> <t>" - Sets initial navigation coordinates and theta
+# "start point <x> <y> <t>" - Sets initial navigation coordinates (meters) and theta (deg)
 # "stop point"            - Stops navigation to point
 # "vel <v> <omega>"       - Sets linear (v) and angular (omega) velocity
 # "point <x> <y>"         - Moves robot to specific XY coordinates
@@ -49,112 +49,112 @@ gripper_location = [ 0.0, 0.0, 0.0] # + some location
 
 
 async def race():
-    ### Race stages
+### Race stages
 
-    ## stage 1 - Start
+## stage 1 - Start
 
-        # wait for DTruck and DFire to be ready
-        if not await DTruckRadio.wait_for_command("ready"):
-                print("timeout while waiting for DTruck to be ready")
+    # wait for DTruck and DFire to be ready
+    if not await DTruckRadio.wait_for_command("ready"):
+            print("timeout while waiting for DTruck to be ready")
 
-        if not await DFireRadio.wait_for_command("ready"):
-                print("timeout while waiting for DFire to be ready")
+    if not await DFireRadio.wait_for_command("ready"):
+            print("timeout while waiting for DFire to be ready")
 
-        # wait for start signal from DT
-        if not await DTruckRadio.wait_for_command("start"):
-                print("timeout while waiting for DTruck to be start")
+    # wait for start signal from DT
+    if not await DTruckRadio.wait_for_command("start"):
+            print("timeout while waiting for DTruck to be start")
 
-        # send start following the line signal to DTruck and DF
-        DTruckRadio.send_command("start")
-        DFireRadio.send_command("start")
+    # send start following the line signal to DTruck and DF
+    DTruckRadio.send_command("start")
+    DFireRadio.send_command("start")
 
-    ## stage 2 - reach point A
+## stage 2 - reach point A
 
-        # wait for "Reached point A" signals from DTruck and DF
-        if not await DTruckRadio.wait_for_command("reached A"):
-                print("timeout while waiting for DTruck to reach A")
+    # wait for "Reached point A" signals from DTruck and DF
+    if not await DTruckRadio.wait_for_command("reached A"):
+            print("timeout while waiting for DTruck to reach A")
 
-        if not await DFireRadio.wait_for_command("reached A"):
-                print("timeout while waiting for DFire to reach A")
+    if not await DFireRadio.wait_for_command("reached A"):
+            print("timeout while waiting for DFire to reach A")
 
-        # Request DTruck current location and store it
-        DTruckRadio.send_command("getpos")
-        success, lineLocation = DTruckRadio.wait_for_response("getpos")
+    # Request DTruck current location and store it
+    DTruckRadio.send_command("getpos")
+    success, lineLocation = DTruckRadio.wait_for_response("getpos")
 
-        if not success:
-            print("timeout while waiting for DFire to reach A")        
+    if not success:
+        print("timeout while waiting for DFire to reach A")        
 
-        # Send command to rotate 90 degrees to the Left to DTruck
-        DTruckRadio.send_command("rotate 90")
-        await asyncio.sleep(2)
+    # Send command to rotate 90 degrees to the Left to DTruck
+    DTruckRadio.send_command("rotate 90")
+    await asyncio.sleep(2)
 
-        # Turn on camera and send local coordinates of an Aruco marker to DTruck every frame
-        arucoTracking = ArucoTracking()
-        
-        arucoPosCameraFrame = arucoTracking.get_marker_position()
-        # TODO: convert arucoPosCameraFrame to global coordinates using camera_location and lineLocation and send to DTruck
+    # Turn on camera and send local coordinates of an Aruco marker to DTruck every frame
+    arucoTracking = ArucoTracking()
+    
+    arucoPosCameraFrame = arucoTracking.get_marker_position()
+    # TODO: convert arucoPosCameraFrame to global coordinates using camera_location and lineLocation and send to DTruck
 
-        # wait for "reached point B" signal from DTruck
-        if not await DTruckRadio.wait_for_command("reached B"):
-                print("timeout while waiting for DTruck to reach B")
+    # wait for "reached point B" signal from DTruck
+    if not await DTruckRadio.wait_for_command("reached B"):
+            print("timeout while waiting for DTruck to reach B")
 
-        # check that the location of the Aruco marker is within a certain distance from the location of DTruck at point A
-        # TODO: do the check by making sure the tracker position is correct to within some margin of error 
+    # check that the location of the Aruco marker is within a certain distance from the location of DTruck at point A
+    # TODO: do the check by making sure the tracker position is correct to within some margin of error 
 
-    ## stage 3 - the hand over
-        # send command to DFire to close the gripper
-        DFireRadio.send_command("close grip")
+## stage 3 - the hand over
+    # send command to DFire to close the gripper
+    DFireRadio.send_command("close grip")
 
-        # wait for "Gripper closed" signal from DFire
-        if not await DFireRadio.wait_for_command("gripper closed"):
-                print("timeout while waiting for DFire to close gripper")
+    # wait for "Gripper closed" signal from DFire
+    if not await DFireRadio.wait_for_command("gripper closed"):
+            print("timeout while waiting for DFire to close gripper")
 
-        # send command to DTruck to open the gripper
-        DTruckRadio.send_command("open grip")
+    # send command to DTruck to open the gripper
+    DTruckRadio.send_command("open grip")
 
-        # wait for "Gripper opened" signal from DT
-        if not await DTruckRadio.wait_for_command("gripper open"):
-                print("timeout while waiting for DTruck to open gripper")
+    # wait for "Gripper opened" signal from DT
+    if not await DTruckRadio.wait_for_command("gripper open"):
+            print("timeout while waiting for DTruck to open gripper")
 
-        # Send command to DTruck to move back a certain distance
-        DTruckRadio.send_command("move -500")
+    # Send command to DTruck to move back a certain distance
+    DTruckRadio.send_command("move -500")
 
-        await asyncio.sleep(2)
+    await asyncio.sleep(2)
 
-    ## stage 4 - reach the finish line 
-        # Send command to DTruck to rotate 90 degrees to the Left
-        DTruckRadio.send_command("rotate 90")
+## stage 4 - reach the finish line 
+    # Send command to DTruck to rotate 90 degrees to the Left
+    DTruckRadio.send_command("rotate 90")
 
-       # Send command to DFire to rotate 180 degrees to the Left
-        DFireRadio.send_command("rotate 180")
-        await asyncio.sleep(2)
+    # Send command to DFire to rotate 180 degrees to the Left
+    DFireRadio.send_command("rotate 180")
+    await asyncio.sleep(2)
 
-        # Send command to DFire to follow the line until the finish line is reached
-        DFireRadio.send_command("start track")
+    # Send command to DFire to follow the line until the finish line is reached
+    DFireRadio.send_command("start track")
 
-        # TODO: there is going to be need for line seeking protocol
-        # send command to move to last known line pos
-        DTruckRadio.send_command("start point " + lineLocation)
+    # TODO: there is going to be need for line seeking protocol
+    # send command to move to last known line pos
+    DTruckRadio.send_command("start point " + lineLocation)
 
-        
-        if not await DTruckRadio.wait_for_command("start point reached"):
-                print("timeout while waiting for 'start point reached'")
+    
+    if not await DTruckRadio.wait_for_command("start point reached"):
+            print("timeout while waiting for 'start point reached'")
 
-        # Send command to DTruck to follow the line until the finish line is reached
-        DTruckRadio.send_command("start track")
+    # Send command to DTruck to follow the line until the finish line is reached
+    DTruckRadio.send_command("start track")
 
- 
-        # wait for "Reached finish line" signal from DTruck and DFire
-        if not await DTruckRadio.wait_for_command("reached C"):
-                print("timeout while waiting for DTruck to reach C")
 
-        if not await DFireRadio.wait_for_command("reached C"):
-                print("timeout while waiting for DFire to reach C")
+    # wait for "Reached finish line" signal from DTruck and DFire
+    if not await DTruckRadio.wait_for_command("reached C"):
+            print("timeout while waiting for DTruck to reach C")
 
-        # Send command to DTruck and DFire to "Celebrate"
-        await asyncio.sleep(2)
-        DFireRadio.send_command("celebrate")
-        DTruckRadio.send_command("celebrate")
+    if not await DFireRadio.wait_for_command("reached C"):
+            print("timeout while waiting for DFire to reach C")
+
+    # Send command to DTruck and DFire to "Celebrate"
+    await asyncio.sleep(2)
+    DFireRadio.send_command("celebrate")
+    DTruckRadio.send_command("celebrate")
 
 
 
